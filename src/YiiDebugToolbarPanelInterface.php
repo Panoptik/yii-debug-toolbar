87a7927831e7808f2f8e96caaab8a18058aa0848
Alexander Golovko <alexander.golovko@comodo.od.ua>
2016-06-16 13:15:14 +0300

    Replace interface into separate file

---
+++ src/YiiDebugToolbarPanelInterface.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author: Alexander Golovko <alexander.golovko@comodo.od.ua>
+ * @created: 16.06.16 13:13
+ */
+
+
+namespace Panoptik\yiidebug;
+
+
+/**
+ * YiiDebugToolbarPanelInterface
+ *
+ * @author Sergey Malyshev <malyshev.php@gmail.com>
+ * @author Igor Golovanov <igor.golovanov@gmail.com>
+ * @version $Id$
+ * @package YiiDebugToolbar
+ * @since 1.1.7
+ */
+interface YiiDebugToolbarPanelInterface
+{
+    /**
+     * Get the title of menu.
+     *
+     * @return string
+     */
+    function getMenuTitle();
+
+    /**
+     * Get the subtitle of menu.
+     *
+     * @return string
+     */
+    function getMenuSubTitle();
+
+    /**
+     * Get the title.
+     *
+     * @return string
+     */
+    function getTitle();
+
+    /**
+     * Get the subtitle.
+     *
+     * @return string
+     */
+    function getSubTitle();
+}
